393d2eba00ed0f89fe626bbcd8ec85292dd57747
Ezekiel <zekejenyart@gmail.com>
2020-07-16 16:42:04 -0500

    heroku

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const writeFileAsync = promisify(fs.writeFile);
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -20,77 +20,127 @@ app.use(express.json());
 
 // =================== ROUTES =====================
 
-app.get("/api/notes", function (req, res) {
-    readFileAsync("./db/db.json", 'utf8', function (err, data) {
-        if (err) return res.sendStatus(500)
+app.get("/api/notes", async function (req, res) {
 
-        data = JSON.parse(data);
-
+    try {
+        let data = await readFileAsync("./db/db.json", 'utf8')
+        data = data.length ? JSON.parse(data) : [];
         res.json(data)
-    })
+    } catch (error) {
+        res.sendStatus(500)
+    }
+
+    // readFileAsync("./db/db.json", 'utf8')
+    //     .then(data => {
+    //         data = data.length ? JSON.parse(data) : [];
+
+    //         res.json(data)
+    //     })
+    //     .catch(() => res.sendStatus(500))
+
+    // readFileAsync("./db/db.json", 'utf8', function (err, data) {
+    //     if (err) return res.sendStatus(500)
+
+
+    //     data = data.length ? JSON.parse(data) : [];
+
+    //     res.json(data)
+    // })
 })
 
-app.post("/api/notes", function (req, res) {
-    readFileAsync("./db/db.json", 'utf8', function (err, data) {
-        // if (err) return res.sendStatus(500)
+app.post("/api/notes", async function (req, res) {
+    try {
+        let data = await readFileAsync("./db/db.json", 'utf8')
 
-        data = JSON.parse(data);
+        data = data.length ? JSON.parse(data) : [];
+        req.body.id = data[data.length - 1].id + 1;
+        data.push(req.body);
+        data = JSON.stringify(data);
 
-        // TODO
-        for (let i = 0; i < data.length; i++) {
-            (data[i]).id = i;
-            
-        }
-        data.push(req.body)
+        await writeFileAsync(path.join(__dirname, "db/db.json"), data)
 
-        // data = JSON.stringify(data);
+        res.json(data)
+    } catch (error) {
+        res.sendStatus(500)
+    }
 
-        return writeFileAsync((path.join(__dirname, "db/db.json")), JSON.stringify(data))
-        .then(function () {
-          res.json(data)
-        })
-            console.log(data, "data read")
-        })
-    })
+    // readFileAsync("./db/db.json", 'utf8', function (err, data) {
+    //     if (err) return res.sendStatus(500)
 
+    //     data = data.length ? JSON.parse(data) : [];
 
-app.delete("/api/notes/:id", function(req, res){
-    let id = req.params.id
-    readFileAsync("./db/db.json", 'utf8')
-    .then(function (data) {
-        // if (err) return res.sendStatus(500)    
-// Parse it so JSON knows what to do
-        console.log("data", data)
-        data = JSON.parse(data)
-        // id = parseInt(id)
-        console.log('ID', id)
-        
-// YOu manipulate it
-        // data.splice(parseInt(id), 1)
-        // data.splice((id), 1)
-// Push the object ot the data
-            // res.json(data)
-        // return writeFileAsync((path.join(__dirname, "db/db.json")), JSON.stringify(data))
-        // .then(function () {
-        //   res.json(data)
-        // })
+    //     // PROPOSITION: each new id is one greater than last elements id
+    //     // req.body.id = data.length;
+    //     req.body.id = data[data.length - 1].id + 1
 
-        return writeFileAsync("./db/db.json", data, function () {
-            if (err) return res.sendStatus(500);
-            // data = JSON.parse(data);
-            // res.sendStatus(200);
-            console.log("data return", data)
-            data = JSON.stringify
-            res.json(data)
+    //     data.push(req.body)
+
+    //     // TODO
+    //     // for (let i = 0; i < data.length; i++) {
+    //     //     data[i].id = i;
+    //     // }
+
+    //     data = JSON.stringify(data);
+
+    //     fs.writeFile(path.join(__dirname, "db/db.json"), data, function (err) {
+    //         if (err) throw err;
+
+    //         // console.log(data, "data read")
+    //         res.json(data)
     //     })
-    //     res.sendStatus(200)
-        })
-    })})
+    // })
+})
+
+
+app.delete("/api/notes/:id", async function (req, res) {
+    try {
+        let id = parseInt(req.params.id)
+        let data = await readFileAsync("./db/db.json", 'utf8')
+
+        data = data.length ? JSON.parse(data) : []
+        data = data.filter(note => note.id !== id)
+        data = JSON.stringify(data)
+
+        await writeFileAsync(path.join(__dirname, "db/db.json"), data)
+
+        res.json(data)
+    } catch (error) {
+        res.sendStatus(500)
+    }
+    // readFileAsync("./db/db.json", 'utf8', function (err, data) {
+    //     if (err) return res.sendStatus(500)
+
+    //     data = data.length ? JSON.parse(data) : []
+
+    //     // You manipulate it
+    //     // data.splice(id, 1)
+
+    //     // TODO
+    //     // for (let i = 0; i < data.length; i++) {
+    //     //     data[i].id = i;
+    //     // }
+
+    //     data = data.filter(note => note.id !== id)
+
+    //     data = JSON.stringify(data)
+
+    //     fs.writeFile("./db/db.json", data, function (err) {
+    //         if (err) return res.sendStatus(500);
+    //         // data = JSON.parse(data);
+    //         // res.sendStatus(200);
+    //         // console.log("data return", data)
+    //         // data = JSON.stringify
+    //         res.json(data)
+    //         //     })
+    //         //     res.sendStatus(200)
+    //     })
+    // })
+})
 
 
 // assign newNote a route name to be able to reference each one specifically
-    // Lowercase no spaces
-    // Use Title key in the objectas a starting point to make route name
+// Lowercase no spaces
+// Use Title key in the objectas a starting point to make route name
 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"))
